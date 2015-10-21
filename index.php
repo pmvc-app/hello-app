fc816006a8b9ad288c12618759890f231aa3fd3b
Hill <hill@kimo.com>
2015-10-21 16:53:26 +0400

    update format

--- index.php
+++ index.php
@@ -5,23 +5,29 @@ ${_INIT_CONFIG}[_CLASS] = 'NewActionName';
 ${_INIT_CONFIG}[_INIT_BUILDER] = $b;
 
 $b->addAction('index', array(
-    _FUNCTION=>array('NewActionName','index')
-    ,_FORM=>'HelloVerify'
+    _FUNCTION => array(
+        ${_INIT_CONFIG}[_CLASS],
+        'index'
+    )
+    ,_FORM => 'HelloVerify'
 ));
 
 $b->addAction('lazy-index', array(
-    _FUNCTION=>array('NewActionName','index_laziness')
+    _FUNCTION => array(
+        ${_INIT_CONFIG}[_CLASS],
+        'index_laziness'
+    )
 ));
 
 $b->addForward('home', array(
-    _PATH=>'hello'
-    ,_TYPE=>'view'
-    ,_LAZY_OUTPUT=>'lazy-index'
+    _PATH => 'hello'
+    ,_TYPE => 'view'
+    ,_LAZY_OUTPUT => 'lazy-index'
 ));
 
 $b->addForward('laze', array(
-    _PATH=>'laze'
-    ,_TYPE=>'view'
+    _PATH => 'laze'
+    ,_TYPE => 'view'
 ));
 
 class NewActionName extends PMVC\Action
